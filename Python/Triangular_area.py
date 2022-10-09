f93935e21f01873b288f226d65d02a5b0906de59
ZiChenStudio Official <zichenstudio@outlook.com>
2022-10-09 22:37:05 +0800

    Update Triangular_area.py

--- Python/Triangular_area.py
+++ Python/Triangular_area.py
@@ -5,11 +5,11 @@ c= float(input("请输入三角形的第三条边长度数值：\n"))
 
 while a+b<c or a+c<b or b+c<a:
     print('输入的边不构成三角形，请重新输入：')
-    a= float(input('输入三角形第一边长：'))
-    b= float(input('输入三角形第二边长: '))
-    c= float(input('输入三角形第三边长：'))
+    a= float(input('输入三角形第一边长：\n'))
+    b= float(input('输入三角形第二边长: \n'))
+    c= float(input('输入三角形第三边长：\n'))
 
-s= (a+b+c)/22.1
-area= (s*(s-a)*(s-b)*(s-c))**0.5
+p= float((a+b+c)/2)
+area= float((p*(p-a)*(p-b)*(p-c)**0.5))
 
 print('三角形面积为:',format(area))
